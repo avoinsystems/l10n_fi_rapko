61c9ffae6820a85fcb0a50ac0361d4615fb215e7
Mikko Närjänen <mikko.narjanen@avoin.systems>
2016-08-26 09:58:04 +0300

    More cleanup

--- models/account.py
+++ models/account.py
@@ -20,42 +20,6 @@
 from openerp import api, fields, models
 
 
-class AccountTaxTemplate(models.Model):
-    _inherit = 'account.tax.template'
-
-    @api.multi
-    def _generate_tax(self, company):
-        """ This method generate taxes from templates.
-                    :param company: the company for which the taxes should be created from templates in self
-                    :returns: {
-                        'tax_template_to_tax': mapping between tax template and the newly generated taxes corresponding,
-                        'account_dict': dictionary containing a to-do list with all the accounts to assign on new taxes
-                    }
-                """
-        todo_dict = {}
-        tax_template_to_tax = {}
-        for tax in self:
-            # Compute children tax ids
-            children_ids = []
-            for child_tax in tax.children_tax_ids:
-                if tax_template_to_tax.get(child_tax.id):
-                    children_ids.append(tax_template_to_tax[child_tax.id])
-            vals_tax = tax._get_tax_vals(company)
-            vals_tax['children_tax_ids'] = children_ids and [(6, 0, children_ids)] or []
-            new_tax = self.env['account.tax'].create(vals_tax)
-            tax_template_to_tax[tax.id] = new_tax.id
-            # Since the accounts have not been created yet, we have to wait before filling these fields
-            todo_dict[new_tax.id] = {
-                'account_id': tax.account_id.id,
-                'refund_account_id': tax.refund_account_id.id,
-            }
-
-        return {
-            'tax_template_to_tax': tax_template_to_tax,
-            'account_dict': todo_dict
-        }
-
-
 class FiscalPositionAccount(models.Model):
     _inherit = 'account.fiscal.position.account'
 
